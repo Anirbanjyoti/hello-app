da29ca533c5642933029ae05a57df2ebe99694a0
Anirbanjyoti <anirbanjyoti708@gmail.com>
2022-09-01 17:08:12 +0530

    db connection secrete

--- database/connect.js
+++ database/connect.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 mongoose
   .connect(
     // `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ov3k2vn.mongodb.net/?retryWrites=true&w=majority/mongooseTest`,
-    "mongodb://localhost:27017/hello-app",
+    process.env.MONGO_CONNECTION_STRING,
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
